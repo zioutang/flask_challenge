a8fa09549d731d2521f6c0cff2ac44421b513e7c
zioutang <tangziou@gmail.com>
2018-01-02 23:43:34 -0800

    test

--- test.py
+++ test.py
@@ -7,19 +7,15 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test1(self):
         response = requests.post('https://morning-dusk-64545.herokuapp.com/upload/1.png');
-        # self.response = requests.post('/upload/IMG_2342.JPG', follow_redirects=True)
         self.assertEqual(response.status_code, 200);
     def test2(self):
         response = requests.post('https://morning-dusk-64545.herokuapp.com/upload/2.png');
-        # self.response = requests.post('/upload/IMG_2342.JPG', follow_redirects=True)
         self.assertEqual(response.status_code, 200);
     def test3(self):
-        response = requests.post('https://morning-dusk-64545.herokuapp.com/upload/3.jpg');
-        # self.response = requests.post('/upload/IMG_2342.JPG', follow_redirects=True)
+        response = requests.post('https://morning-dusk-64545.herokuapp.com/upload/3.JPG');
         self.assertEqual(response.status_code, 200);
     def test4(self):
-        response = requests.post('https://morning-dusk-64545.herokuapp.com/upload/4.jpg');
-        # self.response = requests.post('/upload/IMG_2342.JPG', follow_redirects=True)
+        response = requests.post('https://morning-dusk-64545.herokuapp.com/upload/4.JPG');
         self.assertEqual(response.status_code, 200);
 
 if __name__ == '__main__':
