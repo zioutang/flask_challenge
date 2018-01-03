d119eac8aaeac9574d6987e17d927dbf74a544da
zioutang <tangziou@gmail.com>
2018-01-02 23:19:49 -0800

    test

--- test.py
+++ test.py
@@ -1,21 +1,9 @@
-# import os
-# import flaskr
+
 import requests
 import unittest
-# import tempfile
-# import app
+
 
 class FlaskrTestCase(unittest.TestCase):
-    # def setUp(self):
-    #     self.db_fd, flaskr.app.config['DATABASE'] = tempfile.mkstemp()
-    #     flaskr.app.testing = True
-    #     self.app = flaskr.app.test_client()
-    #     with flaskr.app.app_context():
-    #         flaskr.init_db()
-    #
-    # def tearDown(self):
-    #     os.close(self.db_fd)
-    #     os.unlink(flaskr.app.config['DATABASE'])
 
     def test1(self):
         response = requests.post('http://localhost:3000/upload/1.png');
