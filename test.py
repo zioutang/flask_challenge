254e10086c16d9a80700fab0a18c59266887b4cf
zioutang <tangziou@gmail.com>
2018-01-02 23:32:50 -0800

    123

--- test.py
+++ test.py
@@ -6,19 +6,19 @@ import unittest
 class FlaskrTestCase(unittest.TestCase):
 
     def test1(self):
-        response = requests.post('http://localhost:3000/upload/1.png');
+        response = requests.post('https://morning-dusk-64545.herokuapp.com/upload/1.png');
         # self.response = requests.post('/upload/IMG_2342.JPG', follow_redirects=True)
         self.assertEqual(response.status_code, 200);
     def test2(self):
-        response = requests.post('http://localhost:3000/upload/2.png');
+        response = requests.post('https://morning-dusk-64545.herokuapp.com/upload/2.png');
         # self.response = requests.post('/upload/IMG_2342.JPG', follow_redirects=True)
         self.assertEqual(response.status_code, 200);
     def test3(self):
-        response = requests.post('http://localhost:3000/upload/3.jpg');
+        response = requests.post('https://morning-dusk-64545.herokuapp.com/upload/3.jpg');
         # self.response = requests.post('/upload/IMG_2342.JPG', follow_redirects=True)
         self.assertEqual(response.status_code, 200);
     def test4(self):
-        response = requests.post('http://localhost:3000/upload/4.jpg');
+        response = requests.post('https://morning-dusk-64545.herokuapp.com/upload/4.jpg');
         # self.response = requests.post('/upload/IMG_2342.JPG', follow_redirects=True)
         self.assertEqual(response.status_code, 200);
 
