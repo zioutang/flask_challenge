8daa8585bdf1b2e865bfa15999d8abcbc09018e6
zioutang <tangziou@gmail.com>
2018-01-03 16:07:54 -0800

    test

--- test.py
+++ test.py
@@ -6,19 +6,19 @@ import unittest
 class FlaskrTestCase(unittest.TestCase):
 
     def test1(self):
-        files = {'file': open('images/1.png')}
+        files = {'file': open('test/1.png')}
         response = requests.post('https://morning-dusk-64545.herokuapp.com/upload',files=files);
         self.assertEqual(response.status_code, 200);
     def test2(self):
-        files = {'file': open('images/2.png')}
+        files = {'file': open('test/2.png')}
         response = requests.post('https://morning-dusk-64545.herokuapp.com/upload',files=files);
         self.assertEqual(response.status_code, 200);
     def test3(self):
-        files = {'file': open('images/3.JPG')}
+        files = {'file': open('test/3.JPG')}
         response = requests.post('https://morning-dusk-64545.herokuapp.com/upload',files=files);
         self.assertEqual(response.status_code, 200);
     def test4(self):
-        files = {'file': open('images/4.JPG')}
+        files = {'file': open('test/4.JPG')}
         response = requests.post('https://morning-dusk-64545.herokuapp.com/upload',files=files);
         self.assertEqual(response.status_code, 200);
 
